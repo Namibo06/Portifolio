aab5f302f9901f08233e97ee100ecdd8e8a8bc28
Namibo06 <namibo654@gmail.com>
2024-08-19 13:46:47 -0300

    remove footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AppComponent } from './app.component';
 import { HomeModule } from './pages/home/home.module';
 import { ProjectsModule } from './pages/projects/projects.module';
 import { HeaderModule } from './shared/header/header.module';
-import { FooterModule } from './shared/footer/footer.module';
 import { ContactModule } from './pages/contact/contact.module';
 import { ErrorModule } from './pages/error/error.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -23,7 +22,6 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     HomeModule,
     ProjectsModule,
     HeaderModule,
-    FooterModule,
     ContactModule,
     ErrorModule,
     FontAwesomeModule,
